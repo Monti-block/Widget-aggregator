983fbe20543652ef0ca08a4229403446f68384c8
Monti4 <livien.monti@gmail.com>
2022-12-26 15:24:25 +0100

    added dot

--- src/App.js
+++ src/App.js
@@ -8,8 +8,63 @@ function App() {
       
       <div className="widget">
       <SwapWidget
-      colorMode="dark"
       
+      
+      theme={{
+        palette: {
+          // light | dark
+          mode: 'light',
+          primary: {
+            // main color, noir #1A1A1B
+            main: '#1A1A1B',
+          },
+          secondary: {
+            // secondary color（used for buttons or some window‘s background colors）
+            main: '#0857ee', //bleu
+            contrastText: '#1A1A1B',
+          },
+          error: {
+            main: '#EC5A7D',
+            contrastText: '#FFFFFF',
+          },
+          warning: {
+            main: '#B15600',
+            contrastText: '#1A1A1B',
+          },
+          success: {
+            main: '#2FBA90',
+            contrastText: '#1A1A1B',
+          },
+          purple: {
+            main: '#6851B4',
+            contrastText: '#1A1A1B',
+          },
+          background: {
+            default: '#F9F6E8', //F9F6E8
+            paper: '#FFFFFF',
+            paperContrast: '#F6F6F6',
+            backdrop: 'rgba(0, 0, 0, 0.9)',
+            input: '#F0F0F0',
+            tag: 'rgba(26, 26, 27, 0.1)',
+          },
+          text: {
+            primary: '#1A1A1B',
+            secondary: 'rgba(26, 26, 27, 0.5)',
+            disabled: 'rgba(26, 26, 27, 0.3)',
+            placeholder: 'rgba(26, 26, 27, 0.3)',
+            link: '#1A1A1B',
+          },
+          border: {
+            main: 'rgba(255, 255, 255)', //69, 72, 81, 0.1
+            light: 'rgba(26, 26, 27, 0.3)', //26, 26, 27, 0.3
+            disabled: 'rgba(26, 26, 27, 0.1)',// / 26, 26, 27, 0.1
+          },
+          hover: {
+            default: 'rgba(26, 26, 27, 0.04)',
+          },
+      },
+    }}
+
       jsonRpcUrlMap={{
         1: [
           'https://rpc.ankr.com/eth',
